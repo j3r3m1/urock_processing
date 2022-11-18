ed198925f7b9b49cf81d6cac5fd7222182a9b840
jeremy <jeremy.bernard@zaclys.net>
2022-11-18 08:34:41 +0100

    Drop primary key if duplicated in input table

--- loadData.py
+++ loadData.py
@@ -131,6 +131,7 @@ def loadData(fromCad                        , prefix,
             if idFieldBuild is None or idFieldBuild == "":
                 cursor.execute(""" 
                    ALTER TABLE {0} DROP COLUMN IF EXISTS {1};
+                   ALTER TABLE {0} DROP PRIMARY KEY;
                    ALTER TABLE {0} ADD COLUMN {1} SERIAL;
                    """.format( buildTablePreSrid     , ID_FIELD_BUILD))
                 idFieldBuild = ID_FIELD_BUILD
@@ -175,6 +176,7 @@ def loadData(fromCad                        , prefix,
             if idVegetation is None or idVegetation == "":
                 cursor.execute(""" 
                    ALTER TABLE {0} DROP COLUMN IF EXISTS {1};
+                   ALTER TABLE {0} DROP PRIMARY KEY;
                    ALTER TABLE {0} ADD COLUMN {1} SERIAL;
                    """.format( vegTablePreSrid     , ID_VEGETATION))
                 idVegetation = ID_VEGETATION
